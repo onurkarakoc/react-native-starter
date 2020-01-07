1212537515d3fe9ab8233d3fdadee338896db7a3
onurkarakoc <onurrkarakoc@gmail.com>
2020-01-07 20:51:43 +0300

    Buttons are added to homescreen.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Text, StyleSheet, SafeAreaView } from 'react-native';
+import { Text, StyleSheet, SafeAreaView, Button, TouchableOpacity } from 'react-native';
 import ComponentsScreen from './ComponentsScreen';
 
 const HomeScreen = ({ navigation }) => {
   return (
     <SafeAreaView>
-      <ComponentsScreen />
+      <Button 
+      onPress={() => navigation.navigate('Components')}
+      title="Go to Components Demo"/>
+      <Button 
+      title="Go to List Demo"
+      onPress={() => navigation.navigate('List')}/>
     </SafeAreaView>
       
   );
